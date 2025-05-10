bcf3aab0f40f7deefbd2d2037175d41ab40e4235
Manohar-2905 <kumarmanohar6206@gmail.com>
2025-05-11 00:07:32 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,100 +1,59 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
+import React, { useState } from 'react';
+import NavBar2 from './components/NavBar2';
+import News from './components/News';
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+} from 'react-router-dom';
 
-function NavBar2({ selectedCountry, onCountryChange }) {
-  const countries = [
-    { code: 'us', name: 'United States' },
-    { code: 'ae', name: 'United Arab Emirates' },
-    { code: 'ar', name: 'Argentina' },
-    { code: 'at', name: 'Austria' },
-    { code: 'au', name: 'Australia' },
-    { code: 'be', name: 'Belgium' },
-    { code: 'bg', name: 'Bulgaria' },
-    { code: 'br', name: 'Brazil' },
-    { code: 'ca', name: 'Canada' },
-    { code: 'ch', name: 'Switzerland' },
-    { code: 'cn', name: 'China' },
-    { code: 'co', name: 'Colombia' },
-    { code: 'cu', name: 'Cuba' },
-    { code: 'cz', name: 'Czech Republic' },
-    { code: 'de', name: 'Germany' },
-    { code: 'eg', name: 'Egypt' },
-    { code: 'fr', name: 'France' },
-    { code: 'gb', name: 'United Kingdom' },
-    { code: 'gr', name: 'Greece' },
-    { code: 'hk', name: 'Hong Kong' },
-    { code: 'hu', name: 'Hungary' },
-    { code: 'id', name: 'Indonesia' },
-    { code: 'ie', name: 'Ireland' },
-    { code: 'il', name: 'Israel' },
-    { code: 'in', name: 'India' },
-    { code: 'it', name: 'Italy' },
-    { code: 'jp', name: 'Japan' },
-    { code: 'kr', name: 'South Korea' },
-    { code: 'lt', name: 'Lithuania' },
-    { code: 'lv', name: 'Latvia' },
-    { code: 'ma', name: 'Morocco' },
-    { code: 'mx', name: 'Mexico' },
-    { code: 'my', name: 'Malaysia' },
-    { code: 'ng', name: 'Nigeria' },
-    { code: 'nl', name: 'Netherlands' },
-    { code: 'no', name: 'Norway' },
-    { code: 'nz', name: 'New Zealand' },
-    { code: 'ph', name: 'Philippines' },
-    { code: 'pl', name: 'Poland' },
-    { code: 'pt', name: 'Portugal' },
-    { code: 'ro', name: 'Romania' },
-    { code: 'rs', name: 'Serbia' },
-    { code: 'ru', name: 'Russia' },
-    { code: 'sa', name: 'Saudi Arabia' },
-    { code: 'se', name: 'Sweden' },
-    { code: 'sg', name: 'Singapore' },
-    { code: 'si', name: 'Slovenia' },
-    { code: 'sk', name: 'Slovakia' },
-    { code: 'th', name: 'Thailand' },
-    { code: 'tr', name: 'Turkey' },
-    { code: 'tw', name: 'Taiwan' },
-    { code: 'ua', name: 'Ukraine' },
-    { code: 've', name: 'Venezuela' },
-    { code: 'za', name: 'South Africa' }
-  ];
+function App() {
+  const [country, setCountry] = useState('us'); // default to US
 
   return (
     <div>
-      <nav className="navbar navbar-expand-lg bg-body-tertiary fixed-top">
-        <div className="container-fluid">
-          <Link className="navbar-brand" to="/NewsAtlas">NewsAtlas</Link>
-          <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav">
-            <span className="navbar-toggler-icon"></span>
-          </button>
-
-          <div className="collapse navbar-collapse" id="navbarNav">
-            <ul className="navbar-nav me-auto">
-              <li className="nav-item"><Link className="nav-link" to="/NewsAtlas">Home</Link></li>
-              <li className="nav-item"><Link className="nav-link" to="/business">Business</Link></li>
-              <li className="nav-item"><Link className="nav-link" to="/entertainment">Entertainment</Link></li>
-              <li className="nav-item"><Link className="nav-link" to="/health">Health</Link></li>
-              <li className="nav-item"><Link className="nav-link" to="/science">Science</Link></li>
-              <li className="nav-item"><Link className="nav-link" to="/sports">Sports</Link></li>
-              <li className="nav-item"><Link className="nav-link" to="/technology">Technology</Link></li>
-            </ul>
-
-            <select
-              className="form-select w-auto ms-3"
-              value={selectedCountry || 'us'} // default to 'us'
-              onChange={(e) => onCountryChange(e.target.value)}
-            >
-              {countries.map((country) => (
-                <option key={country.code} value={country.code}>
-                  {country.name}
-                </option>
-              ))}
-            </select>
-          </div>
-        </div>
-      </nav>
+      <Router>
+        <NavBar2 selectedCountry={country} onCountryChange={setCountry} />
+        <Routes>
+          <Route
+            exact
+            path="/NewsAtlas"
+            element={<News key={`general-${country}`} pageSize={5} country={country} category="general" />}
+          />
+          <Route
+            exact
+            path="/technology"
+            element={<News key={`technology-${country}`} pageSize={5} country={country} category="technology" />}
+          />
+          <Route
+            exact
+            path="/sports"
+            element={<News key={`sports-${country}`} pageSize={5} country={country} category="sports" />}
+          />
+          <Route
+            exact
+            path="/science"
+            element={<News key={`science-${country}`} pageSize={5} country={country} category="science" />}
+          />
+          <Route
+            exact
+            path="/health"
+            element={<News key={`health-${country}`} pageSize={5} country={country} category="health" />}
+          />
+          <Route
+            exact
+            path="/entertainment"
+            element={<News key={`entertainment-${country}`} pageSize={5} country={country} category="entertainment" />}
+          />
+          <Route
+            exact
+            path="/business"
+            element={<News key={`business-${country}`} pageSize={5} country={country} category="business" />}
+          />
+        </Routes>
+      </Router>
     </div>
   );
 }
 
-export default NavBar2;
+export default App;
